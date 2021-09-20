f800af1f9c1d5471aa9884ba7711646c94545157
Dwayne Rill Jr <dwayne@dwaynerilljr.dev>
2021-09-20 14:53:00 -0400

    Added title screen and conditional rendering Added title screen; Added button to segway into character select screen;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import {characters} from './characters'
 import CharacterSelect from './CharacterSelect';
+import TitleScreen from './TitleScreen';
 
 function App() {
+  const [select, setSelect] = useState(false)
+  const [title, setTitle] = useState(true)
+
+  const renderSelect = () => {
+    setSelect(!select);
+    setTitle(!title);
+  }
 
   return (
-    <div className="grid content-center justify-center h-screen items-center" id="base">
-      <CharacterSelect characters={characters} />
+    <div className="grid content-center justify-items h-screen items-center" id="base">
+      {title ? (<TitleScreen renderSelect={renderSelect} />) : (null)}
+      {select ? (<CharacterSelect characters={characters} />) : (null)}
     </div>
   );
 }
--- src/CharacterSelect.js
+++ src/CharacterSelect.js
@@ -5,6 +5,15 @@ import { useState } from "react";
 const CharacterSelect = (props) => {
     const [characters, setCharacter] = useState(props.characters)
 
+    const hello = () => {
+        setCharacter(characters);
+    }
+
+    const handleSubmit = (e) => {
+        alert(e.target)
+        e.preventDefault();
+    }
+
     return (
         <>
             <header className="text-center text-white text-5xl font-uncial">Select your Character</header>
@@ -13,17 +22,25 @@ const CharacterSelect = (props) => {
                     return (
                         <div key={character.id}>
                             <div className="flex flex-col text-center bg-white border-2 border-blue-400 p-2 my-8">
-                                <img src={character.avatar} />
+                                <img src={character.avatar} alt={character.name} />
                                 <h2>{character.name} the {character.type}</h2>
                                 <p>Attack: <span className="text-blue-600"><strong>{character.attack}</strong></span></p>
                                 <p>Defence: <span className="text-blue-600"><strong>{character.defense}</strong></span></p>
                                 <p>HP: {character.hp}</p>
-                                <button className="bg-green-600 rounded-md hover:bg-green-800 p-2 font-uncial text-white">Select {character.name}</button>
+                                <form action="submit" onSubmit={handleSubmit} className="flex flex-col" id="characterSelect">
+                                    <span className="mt-3"><strong>Select {character.name}?</strong></span>
+                                    <select name="" id="" className="border-2 border-green-300 rounded-sm self-center w-1/2">
+                                        <option value="selected-character">Yes</option>
+                                        <option selected="selected" value="null">No</option>
+                                    </select>
+                                </form>
                             </div>
                         </div>
                     )})}
             </div>
-            <button className="bg-green-600 flex hover:bg-green-800 p-2 font-uncial text-white">Fight with these Characters</button>
+            <div className="flex justify-center">
+                <button form="characterSelect" onClick={hello} type="submit" className="bg-green-600 text-2xl flex hover:bg-green-800 p-2 font-uncial text-white w-1/6">Battle with these Characters</button>
+            </div>
         </>
     )
 }
